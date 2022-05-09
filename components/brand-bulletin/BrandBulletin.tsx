2b2567244a057bd0c3aeb9818ec6714d50358c1f
Dan Groleau <ddgroleau@gmail.com>
2022-05-08 19:15:30 -0600

    Completed brand bulletin.

--- components/brand-bulletin/BrandBulletin.tsx
+++ components/brand-bulletin/BrandBulletin.tsx
@@ -17,6 +17,32 @@ const BrandBulletin = () => {
                     <Link href="/about">Learn More</Link>
                 </div>
             </div>
+            <div className={styles.bulletinDescription}>
+                <span className={styles.descriptionTitle}>Our Promises</span>
+                <div className={styles.description}>
+                    <div className={styles.descriptionText}>
+                        <span className={styles.descriptionSubtitle}>Cruelty Free, Vegetarian + Vegan*-Friendly</span>
+                        <p>
+                        We’re vegetarians here, so we’ll keep it that way with all of our products. 
+                        Protecting animals and their welfare is our top priority, and always will be
+                        </p>
+                    </div>
+                    <div className={styles.descriptionText}>
+                        <span className={styles.descriptionSubtitle}>Hand-Crafted</span>
+                        <p>
+                        Selected, blended, hand crafted & poured products in Denver, Colorado. 
+                        Everything you see was made with love by Rachel herself 
+                        </p>
+                    </div>
+                    <div className={styles.descriptionText}>
+                        <span className={styles.descriptionSubtitle}>Earth Grown</span>
+                        <p>
+                        We care deeply about the environment and only source our 
+                        herbs and base products from sustainable organizations we love and trust
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
