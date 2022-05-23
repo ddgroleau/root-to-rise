6859bde0c5ad5c8b9dfd252128053e26c1d9a881
Dan Groleau <ddgroleau@gmail.com>
2022-05-22 19:31:01 -0600

    Updated heroku github action.

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-// import Footer from '../footer/Footer';
 import Navbar from '../navbar/Navbar';
 import styles from './Layout.module.css';
 
@@ -17,7 +16,6 @@ const Layout = ({pageTitle, metaDescription, children}:any) => {
             <main>
                 {children}
             </main>
-            {/* <Footer/> */}
             <div>
                 {new Date().getFullYear()} | <Link href="https://www.linkedin.com/in/dangroleau/">Dan Groleau</Link>
             </div>
