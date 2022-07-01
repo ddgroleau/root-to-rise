26635c32650a4b92d900924d2e1f69c2aa4658e8
Dan Groleau <ddgroleau@gmail.com>
2022-07-01 11:38:43 -0600

    Updated favicon and home page styles.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,17 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { useState } from 'react';
 import ErrorBoundary from '../components/error-boundary/ErrorBoundary';
+import ScrollToTop from '../components/scroll-to-top/ScrollToTop';
 
 const  MyApp = ({ Component, pageProps }: AppProps) => {
     const [queryClient] = useState(() => new QueryClient());
     return (
         <ErrorBoundary>
             <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
-                <ReactQueryDevtools initialIsOpen={false} />
+                <ScrollToTop>
+                    <Component {...pageProps} />
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </ScrollToTop>
             </QueryClientProvider>
         </ErrorBoundary>
     );
